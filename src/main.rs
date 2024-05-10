30bb3f13f95f8c9a1305483ac6b8e50b8c1a880d
Michael Strobel <mstrobel97@gmail.com>
2024-05-10 13:57:32 +0200

    refactor: move state in separate file

--- src/main.rs
+++ src/main.rs
@@ -1,109 +1,17 @@
+mod state;
+
+use crate::state::{App, Dir, Program};
 use crossterm::{
     event::{self, Event, KeyCode, KeyEventKind, KeyModifiers},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 use ratatui::{prelude::*, widgets::*};
 use std::{
     error::Error,
     io::{self, Stdout},
     process::{Command, Stdio},
 };
-
-enum Dir {
-    Up,
-    Down,
-    Same,
-}
-
-struct App {
-    input: String,
-    programs: Vec<Program>,
-    filtered_programs: Vec<Program>,
-    index: usize,
-}
-
-#[derive(Clone, Debug)]
-struct Program {
-    name: String,
-    cmd: Vec<String>,
-}
-
-impl App {
-    const fn new() -> Self {
-        Self {
-            input: String::new(),
-            programs: vec![],
-            filtered_programs: vec![],
-            index: 0,
-        }
-    }
-
-    fn load_progams(&mut self) {
-        self.programs = vec![
-            Program {
-                name: String::from("Spotify"),
-                cmd: vec![String::from("spotify")],
-            },
-            Program {
-                name: String::from("Discord"),
-                cmd: vec![String::from("discord")],
-            },
-            Program {
-                name: String::from("Neovim"),
-                cmd: vec![String::from("nvim")],
-            },
-        ];
-
-        self.filter();
-    }
-
-    fn filter(&mut self) {
-        let matcher = SkimMatcherV2::default();
-
-        self.filtered_programs = self
-            .programs
-            .clone()
-            .into_iter()
-            .filter(|p| matcher.fuzzy_match(&p.name, &self.input).is_some())
-            .collect();
-
-        self.move_index(Dir::Same);
-    }
-
-    fn enter_char(&mut self, new_char: char) {
-        self.input.insert(self.input.len(), new_char);
-        self.filter();
-    }
-
-    fn delete_char(&mut self) {
-        if !self.input.is_empty() {
-            self.input = self.input.chars().take(self.input.len() - 1).collect();
-            self.filter();
-        }
-    }
-
-    fn move_index(&mut self, dir: Dir) {
-        let len = self.filtered_programs.len();
-
-        if len == 0 {
-            self.index = 0;
-        } else {
-            self.index = match dir {
-                Dir::Up => match self.index == 0 {
-                    true => len - 1,
-                    false => self.index - 1,
-                },
-                Dir::Down => match self.index == len - 1 {
-                    true => 0,
-                    false => self.index + 1,
-                },
-                Dir::Same => self.index.clamp(0, len - 1),
-            }
-        };
-    }
-}
 
 fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>> {
     let mut stdout = io::stdout();
@@ -122,7 +30,7 @@ fn restore_terminal(
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut terminal = setup_terminal()?;
-    let res = run_app(&mut terminal, App::new());
+    let res = run(&mut terminal, App::new());
     restore_terminal(&mut terminal)?;
 
     if let Ok(Some(program)) = res {
@@ -137,7 +45,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<Option<Program>> {
+fn run(
+    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+    mut app: App,
+) -> io::Result<Option<Program>> {
     app.load_progams();
 
     loop {
@@ -151,6 +62,9 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<O
                     }
                     KeyCode::Char('k') if key.modifiers.contains(KeyModifiers::CONTROL) => {
                         app.move_index(Dir::Up);
+                    }
+                    KeyCode::Char('w') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                        app.delete_word();
                     }
                     KeyCode::Up => {
                         app.move_index(Dir::Up);
@@ -186,11 +100,10 @@ fn ui(frame: &mut Frame, app: &App) {
     let [help_area, search_area, programs_area] = vertical.areas(frame.size());
 
     let text = Text::from(Line::from(vec![
-        "Press ".into(),
         "Esc".bold(),
-        " to exit editing, ".into(),
+        " (exit), ".into(),
         "Enter".bold(),
-        " to launch program".into(),
+        " (launch)".into(),
     ]))
     .patch_style(Style::default());
     let help = Paragraph::new(text);
@@ -198,7 +111,7 @@ fn ui(frame: &mut Frame, app: &App) {
 
     let search = Paragraph::new(app.input.as_str())
         .style(Style::default())
-        .block(Block::bordered().title("Search"));
+        .block(Block::bordered().title(" Search "));
     frame.render_widget(search, search_area);
     frame.set_cursor(
         search_area.x + app.input.len() as u16 + 1,
@@ -218,7 +131,7 @@ fn ui(frame: &mut Frame, app: &App) {
         })
         .collect();
 
-    let programs = List::new(programs).block(Block::bordered().title("Programs"));
+    let programs = List::new(programs).block(Block::bordered().title(" Programs "));
 
     frame.render_widget(programs, programs_area);
 }
---
+++ src/state.rs
@@ -0,0 +1,110 @@
+use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
+
+pub enum Dir {
+    Up,
+    Down,
+    Same,
+}
+
+pub struct App {
+    pub input: String,
+    pub programs: Vec<Program>,
+    pub filtered_programs: Vec<Program>,
+    pub index: usize,
+}
+
+#[derive(Clone, Debug)]
+pub struct Program {
+    pub name: String,
+    pub cmd: Vec<String>,
+}
+
+impl App {
+    pub const fn new() -> Self {
+        Self {
+            input: String::new(),
+            programs: vec![],
+            filtered_programs: vec![],
+            index: 0,
+        }
+    }
+
+    pub fn load_progams(&mut self) {
+        self.programs = vec![
+            Program {
+                name: String::from("Spotify"),
+                cmd: vec![String::from("spotify")],
+            },
+            Program {
+                name: String::from("Discord"),
+                cmd: vec![String::from("discord")],
+            },
+            Program {
+                name: String::from("Neovim"),
+                cmd: vec![String::from("nvim")],
+            },
+        ];
+
+        self.filter();
+    }
+
+    fn filter(&mut self) {
+        let matcher = SkimMatcherV2::default();
+
+        self.filtered_programs = self
+            .programs
+            .clone()
+            .into_iter()
+            .filter(|p| matcher.fuzzy_match(&p.name, &self.input).is_some())
+            .collect();
+
+        self.move_index(Dir::Same);
+    }
+
+    pub fn enter_char(&mut self, new_char: char) {
+        self.input.insert(self.input.len(), new_char);
+        self.filter();
+    }
+
+    pub fn delete_char(&mut self) {
+        if !self.input.is_empty() {
+            self.input = self.input.chars().take(self.input.len() - 1).collect();
+            self.filter();
+        }
+    }
+
+    pub fn move_index(&mut self, dir: Dir) {
+        let len = self.filtered_programs.len();
+
+        if len == 0 {
+            self.index = 0;
+        } else {
+            self.index = match dir {
+                Dir::Up => match self.index == 0 {
+                    true => len - 1,
+                    false => self.index - 1,
+                },
+                Dir::Down => match self.index == len - 1 {
+                    true => 0,
+                    false => self.index + 1,
+                },
+                Dir::Same => self.index.clamp(0, len - 1),
+            }
+        };
+    }
+
+    pub fn delete_word(&mut self) {
+        let words: Vec<&str> = self.input.split_whitespace().collect();
+
+        self.input = match words.len() {
+            0 => String::from(""),
+            1 => String::from(""),
+            _ => match words.split_last() {
+                Some((_, rest)) => rest.join(" "),
+                None => String::from(""),
+            },
+        };
+
+        self.filter();
+    }
+}
